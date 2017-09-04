f4d56896fd7acdf42ed3c7e892f09eed6461c852
rakesh2gnit <rakesh2gnit@gmail.com>
2017-09-05 01:37:11 +0530

    Add Firebase Database

---
+++ app/src/main/java/com/knowledgeplanet/admin/android/LoginActivity.java
@@ -0,0 +1,146 @@
+package com.knowledgeplanet.admin.android;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+
+    private static final String TAG = "LoginActivity";
+    private static final int RC_SIGN_IN = 9001;
+
+    private FirebaseAuth mAuth;
+
+    private GoogleApiClient mGoogleApiClient;
+    private Button btnGoogleLogin;
+    private ProgressBar pBar;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                goToMainActivity(null);
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnGoogleLogin = (Button) findViewById(R.id.btn_google_login);
+        pBar = (ProgressBar) findViewById(R.id.pBar);
+        btnGoogleLogin.setOnClickListener(this);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        goToMainActivity(currentUser);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_google_login:
+                signIn();
+                break;
+        }
+    }
+
+    private void goToMainActivity(FirebaseUser user) {
+        if (user != null) {
+            Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(mainIntent);
+            finish();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    // [auth_with_google]
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        pBar.setVisibility(View.VISIBLE);
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            goToMainActivity(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            goToMainActivity(null);
+                        }
+
+                        pBar.setVisibility(View.GONE);
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/knowledgeplanet/admin/android/MainActivity.java
@@ -0,0 +1,224 @@
+package com.knowledgeplanet.admin.android;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.knowledgeplanet.admin.android.model.Post;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Admin on 04-09-2017.
+ */
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
+
+    private static final String TAG = "MainActivity";
+    private static final String REQUIRED = "Required";
+    private FloatingActionButton fabLogout;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+
+    private GoogleApiClient mGoogleApiClient;
+    EditText et_courseName, et_subjectName, et_subSubjectName, et_imageName;
+    Button btn_submit;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        fabLogout = (FloatingActionButton) findViewById(R.id.fab_logout);
+        fabLogout.setOnClickListener(this);
+
+        et_courseName = (EditText) findViewById(R.id.et_courseName);
+        et_subjectName = (EditText) findViewById(R.id.et_subjectName);
+        et_subSubjectName = (EditText) findViewById(R.id.et_subSubjectName);
+        et_imageName = (EditText) findViewById(R.id.et_imageName);
+        btn_submit = (Button) findViewById(R.id.btn_submit);
+
+        btn_submit.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_submit:
+                submitPost();
+                break;
+
+            case R.id.fab_logout:
+                signOut();
+                break;
+        }
+    }
+
+    private void signOut() {
+        // Firebase sign out
+        mAuth.signOut();
+
+        // Google sign out
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        goToLoginActivity(null);
+                    }
+                });
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+    }
+
+    private void goToLoginActivity(FirebaseUser user) {
+        Intent mainIntent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(mainIntent);
+        finish();
+    }
+
+    private void submitPost() {
+        final String courseName = et_courseName.getText().toString().trim();
+        final String subjectName = et_subjectName.getText().toString().trim();
+        final String subSubjectName = et_subSubjectName.getText().toString().trim();
+        final String imageName = et_imageName.getText().toString().trim();
+
+        // courseName is required
+        if (TextUtils.isEmpty(courseName)) {
+            et_courseName.setError(REQUIRED);
+            return;
+        }
+
+        // subjectName is required
+        if (TextUtils.isEmpty(subjectName)) {
+            et_subjectName.setError(REQUIRED);
+            return;
+        }
+
+        // subSubjectName is required
+        if (TextUtils.isEmpty(subSubjectName)) {
+            et_subSubjectName.setError(REQUIRED);
+            return;
+        }
+
+        // imageName is required
+        if (TextUtils.isEmpty(imageName)) {
+            et_imageName.setError(REQUIRED);
+            return;
+        }
+
+        // Disable button so there are no multi-posts
+        setEditingEnabled(false);
+        Toast.makeText(this, "Posting...", Toast.LENGTH_SHORT).show();
+
+        mDatabase.child("posts").child("").addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // Get value
+                        Post post = dataSnapshot.getValue(Post.class);
+
+                        if (post == null) {
+                            // post is null, error out
+                            Log.e(TAG, "User " + post + " is unexpectedly null");
+                            Toast.makeText(MainActivity.this,
+                                    "Error: could not fetch data.",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            if(post.course.equalsIgnoreCase(courseName)) {
+                                Toast.makeText(MainActivity.this,"Already Exist",Toast.LENGTH_SHORT).show();
+                                writeNewPost(courseName, subjectName, subSubjectName, imageName);
+                            }else{
+                                // Write new post
+                                writeNewPost(courseName, subjectName, subSubjectName, imageName);
+                            }
+                        }
+
+                        // Finish this Activity, back to the stream
+                        setEditingEnabled(true);
+                        //finish();
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.w(TAG, "getUser:onCancelled", databaseError.toException());
+                        setEditingEnabled(true);
+                    }
+                });
+        // [END single_value_read]
+    }
+
+    private void setEditingEnabled(boolean enabled) {
+        et_courseName.setEnabled(enabled);
+        et_subjectName.setEnabled(enabled);
+        et_subSubjectName.setEnabled(enabled);
+        et_imageName.setEnabled(enabled);
+        if (enabled) {
+            btn_submit.setVisibility(View.VISIBLE);
+        } else {
+            btn_submit.setVisibility(View.GONE);
+        }
+    }
+
+    private void writeNewPost(String course, String subject, String subsubject, String image) {
+        // Create new post at /user-posts/$userid/$postid and at
+        // /posts/$postid simultaneously
+        String key = mDatabase.child("posts").push().getKey();
+        Post post = new Post(course, subject, subsubject, image);
+        Map<String, Object> postValues = post.toMap();
+
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put("/posts/" + key, postValues);
+        //childUpdates.put("/user-posts/" + userId + "/" + key, postValues);
+
+        mDatabase.updateChildren(childUpdates);
+    }
+}
